06e00f8724a38c5c1701c35c3cd87ea9d6e7c8a3
Khodosov <43482216+Khodosov@users.noreply.github.com>
2021-02-18 13:10:48 +0300

    Немного переработал интерфейс, добавил список соседей

---
+++ who_is_home/lib/pages/user_profile_settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class userProfileSettingsPage extends StatefulWidget {
+  @override
+  _userProfileSettingsPageState createState() => _userProfileSettingsPageState();
+}
+
+class _userProfileSettingsPageState extends State<userProfileSettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
